222a2dccccec725e2f303c3d6808f5a06d8f258b
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-12-23 10:54:58 +0100

    evaluate_many: better detect half-trained or evaluated models

--- evaluate_many.py
+++ evaluate_many.py
@@ -28,19 +28,34 @@ HYBRID_CMD_TEMPLATE = """python3 evaluate_word2mat.py   \
     --output_file=data/{eval_prefix}{name}/evaluation.csv \
     --downstream_tasks SICKEntailment STS15 STS16 MRPC Tense SubjNumber BigramShift CoordinationInversion OddManOut ObjNumber TREC Length Depth"""
 
+def has_file_with_extension(dirname, ext):
+    for f in os.listdir(join(DATA_ROOT, dirname)):
+        if f.endswith(ext):
+            return True
+    return False
+
+
 def main():
     assert os.path.isdir(DATA_ROOT)
 
     notifier = SlackNotifier()
     evals = []
     models = []
+
+    # There may be empty 'model' or 'evaluation' folders, so we double-check
     for f in os.listdir(DATA_ROOT):
         if f.startswith(MODEL_PREFIX):
-            models.append(f[len(MODEL_PREFIX):])
-        if f.startswith(EVAL_PREFIX):
-            evals.append(f[len(EVAL_PREFIX):])
+            if has_file_with_extension(f, '.encoder'):
+                models.append(f[len(MODEL_PREFIX):])
+        elif f.startswith(EVAL_PREFIX):
+            if has_file_with_extension(f, 'evaluation.csv'):
+                evals.append(f[len(EVAL_PREFIX):])
 
-    missing_evals = set(models) - set(evals)
+    missing_evals = sorted(set(models) - set(evals))
+    print('Missing evaluations:')
+    confirmed = []
+    for name in missing_evals:
+        print('- {}'.format(name))
 
     for name in missing_evals:
         template = CNMOW_CMD_TEMPLATE
